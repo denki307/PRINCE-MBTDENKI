26f2d31ec391bd2ccd0f410676e036ccf14b52ee
denki307 <200189507+denki307@users.noreply.github.com>
2026-02-23 15:51:44 +0530

    Update chat join commands for userbot

--- SHUKLAMUSIC/core/userbot.py
+++ SHUKLAMUSIC/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("ITSZSHUKLA")
-                await self.one.join_chat("MASTIWITHFRIENDSXD")
+                await self.one.join_chat("MUSIC_BOT_WORLD")
+                await self.one.join_chat("Jana_Nayagan_Tamil_chat")
             except:
                 pass
             assistants.append(1)
@@ -69,8 +69,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("ITSZSHUKLA")
-                await self.one.join_chat("MASTIWITHFRIENDSXD")
+                await self.two.join_chat("MUSIC_BOT_WORLD")
+                await self.one.join_chat("Jana_Nayagan_Tamil_chat")
             except:
                 pass
             assistants.append(2)
@@ -90,8 +90,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("ITSZSHUKLA")
-                await self.one.join_chat("MASTIWITHFRIENDSXD")
+                await self.three.join_chat("MUSIC_BOT_WORLD")
+                await self.one.join_chat("Jana_Nayagan_Tamil_chat")
             except:
                 pass
             assistants.append(3)
@@ -111,8 +111,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("ITSZSHUKLA")
-                await self.one.join_chat("MASTIWITHFRIENDSXD")
+                await self.four.join_chat("MUSIC_BOT_WORLD")
+                await self.one.join_chat("Jana_Nayagan_Tamil_chat")
             except:
                 pass
             assistants.append(4)
